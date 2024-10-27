2b49bb31698be492cf9683785f7a9f2103f50d4f
GY-code <guy23@mails.tsinghua.edu.cn>
2024-10-27 12:17:59 +0800

    second commit

---
+++ frontend/src/router.js
@@ -0,0 +1,25 @@
+// // import index1 from './page/index.vue';
+// import index2 from './page/index2.vue';
+// import {createRouter, createWebHistory} from "vue-router";
+//
+//
+// const routes = [
+//     // {
+//     //     path: '/',
+//     //     name: 'index1',
+//     //     component: index1
+//     // },
+//     // {
+//     //     path: '/index2',
+//     //     name: 'index2',
+//     //     component: index2
+//     // }
+//     // // ...更多路由配置
+// ]
+//
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes
+// });
+//
+// export default router;
