96004201f69e861862c6e055a4073b9edef3d1ab
GY-code <gu_yi163@163.com>
2024-10-27 11:57:49 +0800

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,246 @@
+<template>
+  <div class="main-container">
+    <div class="frame">
+      <div class="header">
+        <input class="header-input" />
+        <div class="logo">
+          <div class="logo-1"></div>
+          <span class="esorai">ESORAI</span>
+        </div>
+        <div class="menu">
+          <div class="outline-notifications-bell">
+            <div class="magnifying-glass"><div class="vector"></div></div>
+          </div>
+          <div class="outline-notifications-bell-2">
+            <div class="gear"><div class="vector-3"></div></div>
+          </div>
+          <div class="outline-notifications-bell-4">
+            <div class="bell"><div class="vector-5"></div></div>
+          </div>
+          <button class="frame-6"><span class="frame-7">2</span></button>
+          <div class="frame-8"></div>
+        </div>
+      </div>
+      <div class="flex-row-bc">
+        <div class="frame-9">
+          <span class="dashboard">Dasboard</span
+          ><span class="hi-researcher">Hi, Researcher!</span>
+        </div>
+        <button class="filters">
+          <div class="week">
+            <div class="calendar">
+              <div class="calendar-dots"><div class="vector-a"></div></div>
+            </div>
+            <div class="frame-b">
+              <span class="oct-27">Oct 27 </span>
+              <div class="linear-arrows-alt-arrow-down">
+                <div class="vector-c"></div>
+              </div>
+            </div>
+          </div>
+        </button>
+      </div>
+      <div class="flex-row-ffbb">
+        <div class="data">
+          <div class="flex-row-b">
+            <span class="my-data">My data</span
+            ><button class="button-week">
+              <span class="month">Month</span>
+              <div class="linear-arrows-alt"><div class="vector-d"></div></div>
+            </button>
+          </div>
+          <span class="number-citations">Number of citations</span>
+          <div class="flex-row-cd">
+            <div class="days">
+              <div class="line"></div>
+              <div class="m">
+                <div class="rectangle"></div>
+                <span class="jan">Jan</span>
+              </div>
+              <div class="m-e">
+                <div class="rectangle-f"></div>
+                <span class="feb">Feb</span>
+              </div>
+              <div class="m-10">
+                <div class="rectangle-11"></div>
+                <span class="mar">Mar</span>
+              </div>
+              <div class="m-12">
+                <div class="rectangle-13"></div>
+                <span class="apr">Apr</span>
+              </div>
+              <div class="m-14">
+                <div class="rectangle-15"></div>
+                <span class="may">May</span>
+              </div>
+              <div class="m-16">
+                <div class="rectangle-17"></div>
+                <span class="jun">Jun</span>
+              </div>
+              <div class="t">
+                <div class="rectangle-18"></div>
+                <span class="jul">Jul</span>
+              </div>
+              <div class="f">
+                <div class="rectangle-19"></div>
+                <span class="aug">Aug</span>
+              </div>
+              <div class="w">
+                <div class="rectangle-1a"></div>
+                <span class="sep">Sep</span>
+              </div>
+              <div class="t-1b">
+                <div class="rectangle-1c"></div>
+                <span class="oct">Oct</span>
+              </div>
+              <div class="s">
+                <div class="rectangle-1d"></div>
+                <span class="nov">Nov</span>
+              </div>
+              <div class="s-1e">
+                <div class="rectangle-1f"></div>
+                <span class="dec">Dec</span>
+              </div>
+            </div>
+            <div class="numbers">
+              <span class="num-20">20</span><span class="num-15">15</span
+              ><span class="num-10">10</span><span class="num-5">5</span
+              ><span class="num-0">0</span>
+            </div>
+          </div>
+        </div>
+        <div class="report">
+          <div class="flex-column-f">
+            <span class="report-title">Report</span>
+            <div class="frame-20">
+              <div class="heart">
+                <div class="icon-health"><div class="archive"></div></div>
+                <div class="frame-21">
+                  <div class="heart-22">
+                    <span class="historical-reports">Historical Reports</span>
+                  </div>
+                  <span class="num-100">100</span>
+                </div>
+              </div>
+            </div>
+            <div class="cal">
+              <div class="icon-health-23">
+                <div class="activity"><div class="vector-24"></div></div>
+              </div>
+              <div class="details">
+                <div class="heart-25">
+                  <span class="generating">Generating</span>
+                </div>
+                <span class="num-1">1</span>
+              </div>
+            </div>
+          </div>
+          <div class="chart">
+            <div class="progress-pie-chart">
+              <div class="chart-container">
+                <div class="square-fixed-ratio">
+                  <div class="fixed-ratio"><div class="spine"></div></div>
+                  <div class="fixed-ratio-26"><div class="spine-27"></div></div>
+                </div>
+                <div class="total-container">
+                  <span class="value">85%</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-row-adf">
+        <div class="idea">
+          <span class="idea-28">Idea</span>
+          <div class="tasks">
+            <div class="group-18">
+              <div class="det">
+                <div class="icon">
+                  <div class="duck-icon-heart">
+                    <div class="vector-29"></div>
+                  </div>
+                </div>
+                <div class="content">
+                  <span class="my-ideas">My ideas</span>
+                </div>
+              </div>
+            </div>
+            <div class="rectangle-2a"></div>
+            <div class="task">
+              <div class="text">
+                <div class="text-2b">
+                  <div class="duck-icon-star">
+                    <div class="vector-2c"></div>
+                  </div>
+                </div>
+                <div class="text-2d">
+                  <span class="favorite-ideas">Favorite ideas</span>
+                </div>
+              </div>
+            </div>
+            <div class="rectangle-2e"></div>
+          </div>
+        </div>
+        <div class="progress">
+          <span class="experiment-progress">Experiment progress</span>
+          <div class="chart-2f">
+            <div class="progress-pie-chart-30">
+              <div class="chart-container-31">
+                <div class="square-fixed-ratio-32">
+                  <div class="fixed-ratio-33"><div class="spine-34"></div></div>
+                  <div class="fixed-ratio-35"><div class="spine-36"></div></div>
+                </div>
+                <div class="total-container-37">
+                  <span class="value-38">85%</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <span class="click-to-view">Click to view</span>
+        </div>
+        <div class="community">
+          <div class="frame-39">
+            <span class="community-3a">Community</span>
+          </div>
+          <div class="flex-row-fb">
+            <div class="illustration-travels"><div class="travels"></div></div>
+            <div class="frame-3b">
+              <div class="vector-3c"></div>
+              <div class="play"><div class="vector-3d"></div></div>
+            </div>
+            <div class="audio">
+              <span class="looking-for-new-ideas">looking for new ideas！</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="side-bar">
+      <div class="menu-3e">
+        <div class="dashboard-3f">
+          <div class="linear-settings-fine-tuning-widget"></div>
+        </div>
+        <div class="calendar-40">
+          <div class="duck-icon-brush"><div class="vector-41"></div></div>
+        </div>
+        <div class="sport">
+          <div class="person-simple-run"><div class="vector-42"></div></div>
+        </div>
+        <div class="books">
+          <div class="book-open"><div class="vector-43"></div></div>
+        </div>
+        <div class="food">
+          <div class="duck-icon-compass"><div class="vector-44"></div></div>
+        </div>
+      </div>
+      <div class="outline-arrows-action-logout">
+        <div class="vector-45"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="javascript"></script>
+
+<style src="./index.css"></style>
